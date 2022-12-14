dd4128f016a3f87507a49daf3d08414d502b1ed1
tomaandreisacuiu <tomaandreisacuiu@gmail.com>
2022-12-14 11:08:48 +0100

    design changes

--- app/src/main/java/com/example/smartwaterbottle/Activity2.java
+++ app/src/main/java/com/example/smartwaterbottle/Activity2.java
@@ -9,6 +9,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -27,6 +29,8 @@ public class Activity2 extends AppCompatActivity {
         }
         return super.onContextItemSelected(item);
     }
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +77,19 @@ public class Activity2 extends AppCompatActivity {
                 return false;
             }
         });
+
+        button = (Button) findViewById(R.id.editMyDetailsButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
     @Override
---
+++ app/src/main/java/com/example/smartwaterbottle/SettingsActivity.java
@@ -0,0 +1,34 @@
+package com.example.smartwaterbottle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        button = (Button) findViewById(R.id.editMyDetailsButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
+}
