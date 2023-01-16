33c7fbd6e16dcb930f5047366c28a5cdec4e1459
tomaandreisacuiu <toma.sacuiu.16@gmail.com>
2023-01-16 18:28:57 +0100

    Got the names and addresses of the bonded BT device with the phone

--- app/src/main/java/com/example/smartwaterbottle/Activity3.java
+++ app/src/main/java/com/example/smartwaterbottle/Activity3.java
@@ -1,44 +1,50 @@
 package com.example.smartwaterbottle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.UUID;
 
 public class Activity3 extends AppCompatActivity {
-
-//    private static final String DEVICE_NAME = "Water Bottle";
-//    private static final UUID SERVICE_UUID = UUID.fromString("3cfc9609-f2be-4336-a58e-a5010a43559e");
-//    private static final UUID CHARACTERISTIC_UUID = UUID.fromString("8edb60b0-0b93-4403-8e39-44f1abf18e93");
-//
-//    private BluetoothAdapter mBluetoothAdapter;
-//    private BluetoothLeScanner mBluetoothLeScanner;
-//    private ScanCallback mScanCallback;
-//    private BluetoothGatt mBluetoothGatt;
-    private TextView mValueTextView;
 
     // CREATE OBJECTS
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle drawerToggle;
     TextView intakeViewValue;
+    TextView textViewBT;
 
-    private static final int REQUEST_ENABLE_BT = 1;
-    private static final int REQUEST_LOCATION_PERMISSION = 2;
+   private static final int MY_PERMISSIONS_REQUEST_BLUETOOTH = 1;
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -48,12 +54,11 @@ public class Activity3 extends AppCompatActivity {
         return super.onContextItemSelected(item);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
-
-        mValueTextView = findViewById(R.id.textView);
 
         // BEGIN: DRAWER MENU
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -100,7 +105,35 @@ public class Activity3 extends AppCompatActivity {
         intakeViewValue.setText(String.valueOf(intake) + " L");
         // END: Display the suggested water intake level below
 
+        textViewBT = findViewById(R.id.textViewBT1);
+        //textViewBT.setText("NO");
+
+        // Request the BLUETOOTH permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.BLUETOOTH},
+                    MY_PERMISSIONS_REQUEST_BLUETOOTH);
+        } else {
+            // Permission is granted, proceed
+            BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (btAdapter == null) {
+                System.out.println("Device does not support Bluetooth");
+            } else if (!btAdapter.isEnabled()) {
+                System.out.println("Please turn on Bluetooth");
+            } else {
+                System.out.println(btAdapter.getBondedDevices());
+
+                for (BluetoothDevice device : btAdapter.getBondedDevices()) {
+                    System.out.println(device.getName());
+                }
+            }
+
+        }
+
     }
+
 
     @Override
     public void onBackPressed() {
